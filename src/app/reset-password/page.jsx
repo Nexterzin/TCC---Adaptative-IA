c3d0913cc8282adcd294f98df050190a7b4dcda4
Bruno <bruno.vini21@outlook.com>
2025-09-20 21:50:56 -0300

    [UPDATE] Ajuste de codigo recuperação.

--- src/app/reset-password/page.jsx
+++ src/app/reset-password/page.jsx
@@ -1,13 +1,5 @@
 'use client'
 
-import dynamic from 'next/dynamic'
-import Loading from '../Commons/Component/Loading/loading'
-
-const ResetPasswordForm = dynamic(() => import('./ResetPasswordForm'), {
-  ssr: false, 
-  loading: () => <Loading />, 
-});
-
 const ResetPasswordPage = () => {
   return (
     <ResetPasswordForm />
