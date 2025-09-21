84d50c393d6c6c9c8929075c75d4bdd89d5aada5
Bruno <bruno.vini21@outlook.com>
2025-09-20 22:04:27 -0300

    [UPDATE] Ajuste de codigo recuperação 2.

--- src/app/reset-password/page.jsx
+++ src/app/reset-password/page.jsx
@@ -1,15 +1,17 @@
-// src/app/reset-password/page.jsx
 'use client'
 
-import { Suspense } from 'react';
-import ResetPasswordForm from './ResetPasswordForm';
+import dynamic from 'next/dynamic';
 import Loading from '../Commons/Component/Loading/loading';
+
+// Carrega o ResetPasswordForm apenas no cliente
+const ResetPasswordForm = dynamic(() => import('./ResetPasswordForm'), {
+  ssr: false, // Isso desativa o Server-Side Rendering
+  loading: () => <Loading />, // Mostra o loading enquanto o componente é carregado
+});
 
 const ResetPasswordPage = () => {
   return (
-    <Suspense fallback={<Loading />}>
-      <ResetPasswordForm />
-    </Suspense>
+    <ResetPasswordForm />
   );
 };
 
