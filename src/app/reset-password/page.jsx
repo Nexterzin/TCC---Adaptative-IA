4196cd821e5dbd8466689046c80d2bfcb64811a8
Bruno <bruno.vini21@outlook.com>
2025-09-20 22:00:19 -0300

    [UPDATE] Ajuste de codigo recuperação.

--- src/app/reset-password/page.jsx
+++ src/app/reset-password/page.jsx
@@ -1,11 +1,16 @@
+// src/app/reset-password/page.jsx
 'use client'
 
-import ResetPasswordForm from "./ResetPasswordForm"
+import { Suspense } from 'react';
+import ResetPasswordForm from './ResetPasswordForm';
+import Loading from '../Commons/Component/Loading/loading';
 
 const ResetPasswordPage = () => {
   return (
-    <ResetPasswordForm />
-  )
-}
+    <Suspense fallback={<Loading />}>
+      <ResetPasswordForm />
+    </Suspense>
+  );
+};
 
-export default ResetPasswordPage
+export default ResetPasswordPage;
